b05aee758c8360399a71c5590fc613e0702729ce
michalele233 <michal2003p@wp.pl>
2025-02-25 18:46:30 +0100

    Add testing page to see if Steam API works correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-function App() {
+import { useEffect, useState } from "react";
+
+const App = () => {
+	const [gameData, setGameData] = useState(null);
+	const [steamId, setSteamId] = useState("76561198081891605"); // Przykładowy SteamID
+
+	const fetchGameData = async () => {
+		const apiKey = import.meta.env.VITE_APP_STEAM_API_KEY;
+		const response = await fetch(
+			`https://proxy.cors.sh/http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`
+		);
+		const data = await response.json();
+		setGameData(data.response.players[0]);
+	};
+
+	useEffect(() => {
+		fetchGameData();
+	}, [steamId]);
+
 	return (
-		<>
-			<h1 className='text-5xl'>Hello</h1>
-		</>
+		<div className='p-4'>
+			<h1 className='text-3xl font-bold mb-4'>Steam Game Tracker</h1>
+			{gameData ? (
+				<div>
+					<h2 className='text-xl'>{gameData.personaname}</h2>
+					<img
+						src={gameData.avatarfull}
+						alt='Avatar'
+						className='w-32 h-32 rounded-full'
+					/>
+					<p>{gameData.steamid}</p>
+				</div>
+			) : (
+				<p>Loading...</p>
+			)}
+		</div>
 	);
-}
+};
 
 export default App;
